56bbc265dfba2fa714645ca8bf48348641e32c10
July <julys.makarenko@gmail.com>
2022-11-23 18:02:55 +0200

    fix grid

--- src/App.vue
+++ src/App.vue
@@ -18,22 +18,28 @@
         <template v-slot:no-data>No results found</template>
       </v-autocomplete>
     </v-app-bar>
-
-    <div class="container">
-      <section class="movie__list pt-16 ma-8">
-        <div v-for="movie in entries" :key="movie.imdbID" class="ma-2">
+    <v-container class="mt-16 pt-12">
+      <v-row>
+        <v-col
+          v-for="movie in entries"
+          :key="movie.imdbID"
+          cols="12"
+          xs="12"
+          sm="6"
+          md="4"
+          lg="3"
+        >
           <a
             :href="'https://www.imdb.com/title/' + movie.imdbID"
             style="text-decoration: none"
           >
-            <v-card width="350">
+            <v-card w-100>
               <v-img
                 :src="movie.Poster"
                 height="400px"
                 class="pa-4"
                 cover
               ></v-img>
-
               <v-card-title class="pt-4 pb-4">
                 {{ movie.Title }}
               </v-card-title>
@@ -45,9 +51,9 @@
               >
             </v-card>
           </a>
-        </div>
-      </section>
-    </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -59,6 +65,7 @@ export default {
     isLoading: false,
     selectedMovie: null,
     search: "",
+    mobileView: false,
   }),
   mounted() {
     this.fetchMovies("sun");
@@ -91,6 +98,12 @@ export default {
         })
         .finally(() => (this.isLoading = false));
     }, 500),
+
+    handleView() {
+      window.innerWidth <= 428
+        ? (this.tabletView = true)
+        : (this.tabletView = false);
+    },
   },
 };
 </script>
@@ -101,15 +114,5 @@ export default {
   height: 78px;
   box-shadow: 0 2px 4px -1px rgb(0 0 0 / 20%), 0 4px 5px 0 rgb(0 0 0 / 14%),
     0 1px 10px 0 rgb(0 0 0 / 12%);
-}
-.contain {
-  margin: 0 auto;
-}
-
-.movie__list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin: 16px;
 }
 </style>
